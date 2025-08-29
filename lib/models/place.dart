2910d74423af66ccec2d8f00dd5a0cae095643c3
Maruli <tommypurba73@gmail.com>
2025-08-29 19:37:02 +0700

    refactor plaxe.dart

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,18 +1,11 @@
 import 'package:uuid/uuid.dart';
-
-
-
 const uuid = Uuid();
 
 class Favorite{
   Favorite({
-   required this.title,
-    this.map = '',
-    this.img = '',
+   required this.title
   }): id = uuid.v4();
 
   final String id;
   final String title;
-  final String map;
-  final String img;
 }
