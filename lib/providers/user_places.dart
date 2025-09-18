50061e8d37a8aba57d4eb9a48cffd48b39b1dd86
Maruli <tommypurba73@gmail.com>
2025-09-18 14:36:31 +0700

    add loading data from the SQL Database

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -4,6 +4,8 @@ import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 
 class UserPlacesNotifier extends StateNotifier<List<Favorite>> {
@@ -14,6 +16,23 @@ class UserPlacesNotifier extends StateNotifier<List<Favorite>> {
     final fileName = path.basename(image.path);
     final copiedImage = await image.copy('${appDir.path}/$fileName');
     final newPlaces = Favorite(title: title,image: copiedImage, location: location);
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version){
+        return db.execute('CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+      },
+      version: 1,
+    );
+
+    db.insert('user_places', {
+      'id' : newPlaces.id,
+      'title' : newPlaces.title,
+      'image' : newPlaces.image.path,
+      'lat' : newPlaces.location.latitude,
+      'lng' : newPlaces.location.longitude,
+      'address' : newPlaces.location.address,
+    });
     state = [newPlaces, ...state];
   }
 }
